a300f11ac2d79b6c445dbaf300d266daf5a7cf64
Jiahao Huamani <lucashuamani256w@gmail.com>
2023-02-03 04:06:46 -0500

    Implement comparison function
    Made a comparison function playRound() that compares the user choice
    with the randomly computer generated choice and returns the result.

--- script.js
+++ script.js
@@ -13,4 +13,46 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toUpperCase() == "ROCK") {
+        if (computerSelection == "rock") {
+            return "It's a tie!"
+        }
+        else if (computerSelection == "paper") {
+            return "You Lose! Paper beats Rock"
+        }
+        else{
+            return "You Win! Rock Beats Scissors"
+        }
+    }
+    else if (playerSelection.toUpperCase() == "PAPER") {
+        if (computerSelection == "rock") {
+            return "You Win! Paper beats Rock"
+        }
+        else if (computerSelection == "paper") {
+            return "It's a tie!"
+        }
+        else{
+            return "You lose! Scissors Beats Paper"
+        }
+    }
+    else if (playerSelection.toUpperCase() == "SCISSORS") {
+        if (computerSelection == "rock") {
+            return "You Lose! Rock Beats Scissors"
+        }
+        else if (computerSelection == "paper") {
+            return "You Win! Scissors beats Paper"
+        }
+        else{
+            return "It's a tie!"
+        }
+    }
+    else {
+        return "That's not a valid entry!"
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice()
+
+console.log(playRound(playerSelection,computerSelection));
